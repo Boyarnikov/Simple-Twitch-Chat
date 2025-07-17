7590d15f4d890cac72fde68c77c4784801b111c6
boyar <iboyarnikov@gmail.com>
2025-07-17 14:51:08 +0300

    ogo faili

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import TwitchExample
+import asyncio
+import logging
+
+from lib.base import Communicator, Message, Module
+
+from modules import TwitchListenerModule, TTSModule, FlaskModule, LLMModule
+
+
+class StreamCommunicator(Communicator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.init_module(TwitchListenerModule(self))
+        self.init_module(TTSModule(self))
+        self.init_module(FlaskModule(self))
+        self.init_module(LLMModule(self))
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+
+    sc = StreamCommunicator(logger = logger)
+    await asyncio.sleep(100000)
+
 
 if __name__ == "__main__":
-    TwitchExample.start()
+    asyncio.run(main())
